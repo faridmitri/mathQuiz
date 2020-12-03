50f5658246cb8eaf005f5bae74cfae365d85eb6f
Farid Mitri <faridmitri@gmail.com>
2020-12-03 17:31:59 +0200

    timer

--- app/src/main/java/com/am/mathquiz/Game.java
+++ app/src/main/java/com/am/mathquiz/Game.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.view.accessibility.AccessibilityViewCommand;
 
 import android.os.Bundle;
+import android.os.CountDownTimer;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -30,6 +31,10 @@ public class Game extends AppCompatActivity {
     int realanswer;
     int userscore = 0;
     int userlife = 3;
+    CountDownTimer timer;
+    private static final long START_TIMER_IN_MILIS = 30000;
+    boolean timer_running;
+    long TIME_LEFT_IN_MILIS = START_TIMER_IN_MILIS;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -49,8 +54,10 @@ public class Game extends AppCompatActivity {
         ok.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                ok.setEnabled(false);
                 nextquest.setEnabled(true);
                 useranswer = Integer.valueOf(answer.getText().toString());
+                pauseTimer();
                 if (useranswer == realanswer) {
                     userscore = userscore + 1;
                     question.setText("Correct Answer");
@@ -67,17 +74,67 @@ public class Game extends AppCompatActivity {
         nextquest.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                resetTimer();
                 answer.setText("");
-             gameContinue();
+               gameContinue();
+
             }
         });
     }
 
     public void gameContinue() {
+        ok.setEnabled(true);
         nextquest.setEnabled(false);
+        startTimer();
         number1 = random.nextInt(100);
         number2 = random.nextInt(100);
         realanswer = number1 + number2;
         question.setText(number1 + "+" + number2);
+    }
+
+    public void startTimer() {
+        timer = new CountDownTimer(TIME_LEFT_IN_MILIS,1000) {
+            @Override
+            public void onTick(long l) {
+                TIME_LEFT_IN_MILIS = l;
+                updateText();
+            }
+
+            @Override
+            public void onFinish() {
+                timer_running = false;
+                pauseTimer();
+                resetTimer();
+                updateText();
+                userlife = userlife - 1;
+                question.setText("Time is up");
+                ok.setEnabled(false);
+                nextquest.setEnabled(true);
+
+            }
+        }.start();
+        timer_running = true;
+    }
+
+    public void updateText()
+    {
+       int second = (int)(TIME_LEFT_IN_MILIS/1000)%60;
+       //String timeleft = String.format(Local.getDefault(),"%02d",second);
+        String timeleft = (String.format("%02d",second));
+        time.setText(timeleft);
+        life.setText(String.format("%d", userlife));
+
+    }
+
+    public void pauseTimer()
+    {
+       timer.cancel();
+       timer_running = false;
+    }
+
+    public void resetTimer()
+    {
+      TIME_LEFT_IN_MILIS = START_TIMER_IN_MILIS;
+      updateText();
     }
 }
